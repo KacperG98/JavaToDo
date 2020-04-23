5f335dc433cfb7b5a7ef2e0538ccdda65118b4c3
Kacper Gmurczyk <47670007+KacperG98@users.noreply.github.com>
2020-04-23 14:33:56 +0200

    add secturity

---
+++ src/main/java/pl/todolist/tdapi/security/SecurityConfig.java
@@ -0,0 +1,33 @@
+package pl.todolist.tdapi.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = (UserDetails) User.withDefaultPasswordEncoder().username("user").password("user1")
+                .roles("USER");
+        UserDetails admin = (UserDetails) User.withDefaultPasswordEncoder().username("admin").password("admin1")
+                .roles("ADMIN");
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/task/all").permitAll().anyRequest().hasRole("USER").and().formLogin()
+                .permitAll();
+    }
+
+}
