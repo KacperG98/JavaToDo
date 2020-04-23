a9d9ef8d46dc65118af25a60a49de13928442017
Kacper Gmurczyk <47670007+KacperG98@users.noreply.github.com>
2020-04-23 15:18:34 +0200

    fix security and entities

--- src/main/java/pl/todolist/tdapi/entity/Task.java
+++ src/main/java/pl/todolist/tdapi/entity/Task.java
@@ -18,6 +18,10 @@ public class Task {
 
     private boolean isComplete = false;
 
+    public Task() {
+
+    }
+
     public Task(String title, String description) {
         this.title = title;
         this.description = description;
--- src/main/java/pl/todolist/tdapi/security/SecurityConfig.java
+++ src/main/java/pl/todolist/tdapi/security/SecurityConfig.java
@@ -2,10 +2,8 @@ package pl.todolist.tdapi.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,8 +14,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder().username("user").password("user1").roles("USER");
-        UserDetails admin = User.withDefaultPasswordEncoder().username("admin").password("admin1").roles("ADMIN");
+        UserDetails user = User.withDefaultPasswordEncoder().username("user").password("user1").roles("USER").build();
+        UserDetails admin = User.withDefaultPasswordEncoder().username("admin").password("admin1").roles("ADMIN")
+                .build();
 
         return new InMemoryUserDetailsManager(user, admin);
     }
